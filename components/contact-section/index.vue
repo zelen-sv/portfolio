311c1da9f018b649f4b3d100400cc9e9bd164007
zelen-sv-test <st.zelenko.news@gmail.com>
2020-12-14 23:21:41 +0300

    thanks page

--- components/contact-section/index.vue
+++ components/contact-section/index.vue
@@ -5,14 +5,14 @@
       Contact
     </h1>
     <hr class="section__devider">
-    <form name="contact" method="post" data-netlify="true" netlify-honeypot="bot-field">
+    <form name="contact" method="post" action="/message-sent" netlify="true" netlify-honeypot="bot-field">
       <p class="contact-section__bot-input">
         <label>Don’t fill this out if you’re human:
         <input name="bot-field" /></label>
       </p>
-      <input v-model="name" required name="name" placeholder="Name" class="contact-section__input" >
+      <input v-model="name" required name="name" placeholder="Name" type="text" class="contact-section__input" >
       <input v-model="email" required name="email" placeholder="Email" type="email" class="contact-section__input" >
-      <input v-model="message" required name="message" placeholder="Message" class="contact-section__input" >
+      <input v-model="message" required name="message" placeholder="Message" type="text" class="contact-section__input" >
       <v-button class="contact-section__button" text="Send message" type="submit" />
     </form>
   </div>
